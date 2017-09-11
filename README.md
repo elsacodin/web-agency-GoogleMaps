10d55c837995266fcbeb30b0aa9aa5d837be65e5
Elsa Bourgeois <elsa31@gmail.com>
2017-09-12 00:20:57 +0200

    initiate Radar Search of Google Places API web services

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-*Web agency Google Maps*
+#Web agency Google Maps
 
 The purpose of this script is to display all the web agencies on a Paris Maps by a simple use of Google Maps API et more specifically GooglePlaces library.
--- map.js
+++ map.js
@@ -1,12 +1,34 @@
 
   var map;
+  //Defining the window displaying the request 'web agency'
+  var infowindow;
+  // Defining the keywords for filtering the search
+  var searchwords = "agence+web";
 
+  // Initiate Map
   function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-        lat: 48.8704907,
-        lng: 2.3309359
-      },
-      zoom: 12
-    });
-  }
+      var paris = {lat: 48.8704907, lng: 2.3309359};
+      map = new google.maps.Map(document.getElementById('map'), {
+          center: paris,
+          zoom: 13,
+          styles: [{
+              stylers: [{ visibility: 'simplified' }]
+          }, {
+              elementType: 'labels',
+              stylers: [{ visibility: 'off' }]
+          }]
+      });
+
+      infowindow = new google.maps.InfoWindow();
+
+      // Call the Places API Web Service
+    var service = new google.maps.places.PlacesService(map);
+
+    // Initiate Radar Search
+    service.radarSearch({
+        location: paris,
+        radius: 7000,
+        keyword: searchwords
+    }, callback);
+
+}
